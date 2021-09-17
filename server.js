b9c76951b6339fa346205180e28758a0fb92885c
hubbub-inc <matthewcarrella9@gmail.com>
2021-09-17 01:14:25 -0400

    modify socket function

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-'use strict';
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-const express = require('express');
-const socketIO = require('socket.io');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+var clients = 0;
+
+io.on('connection', function(socket) {
+	clients++;
+	io.sockets.emit('broadcast', {description: clients + 'clients connected!'});
+	socket.on('disconnect', function() {
+		clients--;
+		io.sockets.emit('broadcast', {descripton: clients + 'clients connected!'});
+
+	});
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
+
